2aa365804d868038a957f7a03f6d805a0a25c1b0
khoadoantreehouse <khoa.doan@treehouse.finance>
2023-02-14 15:24:20 -0800

    include fflush

--- doankh_program3/main.c
+++ doankh_program3/main.c
@@ -5,10 +5,19 @@
 #define MAX_COMMAND_LENGTH 2048
 #define MAX_ARGUMENTS 512
 
+struct command
+{
+    char *name;
+    char *arguments[MAX_ARGUMENTS];
+    char *input_file;
+    char *output_file;
+    int background;
+};
+
 int main()
 {
-    char command[MAX_COMMAND_LENGTH];
-    char *arguments[MAX_ARGUMENTS];
+    char command_line[MAX_COMMAND_LENGTH];
+    struct command cmd;
     char *token;
     int argument_count;
 
@@ -16,26 +25,55 @@ int main()
     {
         // Print the command prompt
         printf(": ");
+        fflush(stdout);
 
         // Read in the command line
-        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+        if (fgets(command_line, MAX_COMMAND_LENGTH, stdin) == NULL)
         {
             // Exit if an error occurs while reading input
             exit(1);
         }
 
-        // Parse the command line into individual arguments
+        // Initialize the command struct
+        memset(&cmd, 0, sizeof(struct command));
+
+        // Parse the command line into individual arguments and set the command struct members
         argument_count = 0;
-        token = strtok(command, " \n");
+        token = strtok(command_line, " \n");
         while (token != NULL && argument_count < MAX_ARGUMENTS - 1)
         {
-            arguments[argument_count] = token;
-            argument_count++;
+            if (strcmp(token, "<") == 0)
+            {
+                // Input redirection
+                token = strtok(NULL, " \n");
+                cmd.input_file = token;
+            }
+            else if (strcmp(token, ">") == 0)
+            {
+                // Output redirection
+                token = strtok(NULL, " \n");
+                cmd.output_file = token;
+            }
+            else if (strcmp(token, "&") == 0)
+            {
+                // Background process
+                cmd.background = 1;
+            }
+            else
+            {
+                // Command or argument
+                if (argument_count == 0)
+                {
+                    cmd.name = token;
+                }
+                cmd.arguments[argument_count] = token;
+                argument_count++;
+            }
             token = strtok(NULL, " \n");
         }
-        arguments[argument_count] = NULL;
+        cmd.arguments[argument_count] = NULL;
 
-        // TODO: Handle the parsed command and arguments here
+        // TODO: Handle the parsed command using the command struct
     }
 
     return 0;
