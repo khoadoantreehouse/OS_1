cf237d4f2e6007793dd9f79c5f2ab1e602fc1e36
khoadoantreehouse <khoa.doan@treehouse.finance>
2023-02-17 12:09:55 -0800

    may be here ?

--- doankh_program3/main.c
+++ doankh_program3/main.c
@@ -298,6 +298,7 @@ volatile int handleCommand(struct command cmd, int *status)
                 // wait for the child process to finish, if not a background process or in foreground-only mode
                 int child_status;
                 pid_t wpid = waitpid(pid, &child_status, 0);
+                signal(SIGTSTP, handle_SIGTSTP); // Handle SIGTSTP
 
                 *status = child_status;
                 // print signal number if the child was terminated by a signal
