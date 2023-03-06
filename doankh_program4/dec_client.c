799273cd3cd0e4818a417849c632d4eb38d88aac
khoadoantreehouse <khoa.doan@treehouse.finance>
2023-03-05 23:17:25 -0800

    this is not the error

--- doankh_program4/dec_client.c
+++ doankh_program4/dec_client.c
@@ -93,11 +93,11 @@ int main(int argc, char *argv[])
     int port = atoi(argv[3]);
 
     // Validate the input files
-    //
-    // if (validate_file(ciphertext) || validate_file(key) || validate_key_length(ciphertext, key))
-    // {
-    //     exit(1);
-    // }
+
+    if (validate_file(ciphertext) || validate_file(key) || validate_key_length(ciphertext, key))
+    {
+        exit(1);
+    }
 
     // Create a socket and connect to the server
     int sockfd, n;
