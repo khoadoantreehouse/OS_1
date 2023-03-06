740b19c618d3d0fed43ba61fdc64589671398da1
khoadoantreehouse <khoa.doan@treehouse.finance>
2023-03-06 00:01:44 -0800

    test what received

--- doankh_program4/dec_server.c
+++ doankh_program4/dec_server.c
@@ -91,6 +91,8 @@ int main(int argc, char *argv[])
             // verify client identity
             memset(buffer, 0, 2048);
             n = read(clientfd, buffer, 2047);
+            printf("%s\n", buffer);
+
             if (n < 0)
             {
                 error("Error reading from socket");
@@ -98,8 +100,6 @@ int main(int argc, char *argv[])
 
             if (strcmp(buffer, "dec_client") != 0)
             {
-                printf("%s\n", buffer);
-
                 error("Error: not a valid client");
             }
 
