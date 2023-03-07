620a908930ff67064c9d5e76ef407f4ffa587ae1
khoadoantreehouse <khoa.doan@treehouse.finance>
2023-03-06 16:29:50 -0800

    HAVE FUN

--- doankh_program4/enc_server.c
+++ doankh_program4/enc_server.c
@@ -1,180 +1,232 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <netinet/in.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
-#define MAX_CONNECTIONS 5
+#define MAX_CLIENTS 5
+#define BUFFER_SIZE 256
 
-void error(const char *msg)
+void error(char *msg)
 {
-    perror(msg);
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
     exit(1);
 }
 
-char *encrypt(char *message, char *key)
+void validateArgs(int argc, char *argv[])
 {
-    char *ciphertext = (char *)malloc((strlen(message) + 1) * sizeof(char));
-    int i;
-    for (i = 0; i < strlen(message); i++)
-    {
-        int messageVal = message[i] - 'A';
-        int keyVal = key[i] - 'A';
-        if (messageVal == -33)
-            messageVal = 26;
-        if (keyVal == -33)
-            keyVal = 26;
-        int sum = messageVal + keyVal;
-        int cipherVal = sum % 27;
-        ciphertext[i] = cipherVal + 'A';
-        if (cipherVal == 26)
-            ciphertext[i] = ' ';
-    }
-    ciphertext[i] = '\0';
-    return ciphertext;
-}
-
-void error(const char *msg)
-{
-    fprintf(stderr, "%s\n", msg);
-    exit(1);
-}
-
-char *encrypt(char *message, char *key)
-{
-    char *ciphertext = (char *)malloc((strlen(message) + 1) * sizeof(char));
-    int i;
-    for (i = 0; i < strlen(message); i++)
-    {
-        int messageVal = message[i] - 'A';
-        int keyVal = key[i] - 'A';
-        if (messageVal == -33)
-            messageVal = 26;
-        if (keyVal == -33)
-            keyVal = 26;
-        int sum = messageVal + keyVal;
-        int cipherVal = sum % 27;
-        ciphertext[i] = cipherVal + 'A';
-        if (cipherVal == 26)
-            ciphertext[i] = ' ';
-    }
-    ciphertext[i] = '\0';
-    return ciphertext;
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
+    if (argc != 2)
     {
         fprintf(stderr, "Usage: %s port\n", argv[0]);
         exit(1);
     }
-
-    int portno = atoi(argv[1]);
-    if (portno == 0)
+    int port = atoi(argv[1]);
+    if (port < 1024 || port > 65535)
     {
-        error("Invalid port number");
+        fprintf(stderr, "Invalid port number: %s\n", argv[1]);
+        exit(1);
+    }
+}
+
+int setupSocket(int portNum)
+{
+    int serverSock = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSock < 0)
+    {
+        error("Error opening socket");
     }
 
-    int sockfd, newsockfd;
-    socklen_t clilen;
-    struct sockaddr_in serv_addr, cli_addr;
+    struct sockaddr_in serverAddr;
+    bzero((char *)&serverAddr, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(portNum);
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
+    if (bind(serverSock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
     {
-        error("ERROR opening socket");
+        error("Error binding socket");
     }
 
-    bzero((char *)&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
+    listen(serverSock, MAX_CLIENTS);
+    return serverSock;
+}
 
-    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        error("ERROR on binding");
-    }
-
-    listen(sockfd, MAX_CONNECTIONS);
-
-    clilen = sizeof(cli_addr);
+char *encrypt(char *message, char *key)
+{
+    char *ciphertext = (char *)malloc((strlen(message) + 1) * sizeof(char));
     int i;
-    for (i = 0; i < MAX_CONNECTIONS; i++)
+    for (i = 0; i < strlen(message); i++)
     {
-        pid_t pid = fork();
-        if (pid == 0)
+        int messageVal = message[i] - 'A';
+        int keyVal = key[i] - 'A';
+        if (messageVal == -33)
+            messageVal = 26;
+        if (keyVal == -33)
+            keyVal = 26;
+        int sum = messageVal + keyVal;
+        int cipherVal = sum % 27;
+        ciphertext[i] = cipherVal + 'A';
+        if (cipherVal == 26)
+            ciphertext[i] = ' ';
+    }
+    ciphertext[i] = '\0';
+    return ciphertext;
+}
+
+int daemonize()
+{
+    // Fork the process
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        error("ERROR: Could not fork");
+        exit(1);
+    }
+    if (pid > 0)
+    {
+        // Parent process
+        exit(0);
+    }
+
+    // Child process
+    // Set the file mode mask
+    umask(0);
+
+    // Create a new session
+    pid_t sid = setsid();
+    if (sid < 0)
+    {
+        error("ERROR: Could not create new session");
+        exit(1);
+    }
+
+    // Close standard file descriptors
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    return 0;
+}
+
+void handleConnection(int clientSock)
+{
+    char buffer[BUFFER_SIZE];
+    char *clientName = "enc_client";
+    int charsRead;
+
+    // Verify connection is from enc_client
+    memset(buffer, '\0', BUFFER_SIZE);
+    charsRead = recv(clientSock, buffer, BUFFER_SIZE - 1, 0);
+    if (charsRead < 0)
+    {
+        error("ERROR reading from socket");
+        close(clientSock);
+        exit(1);
+    }
+    if (strcmp(buffer, clientName) != 0)
+    {
+        fprintf(stderr, "ERROR: Connection not from %s\n", clientName);
+        close(clientSock);
+        exit(2);
+    }
+
+    // Receive plaintext
+    memset(buffer, '\0', BUFFER_SIZE);
+    charsRead = recv(clientSock, buffer, BUFFER_SIZE - 1, 0);
+    if (charsRead < 0)
+    {
+        error("ERROR reading from socket");
+        close(clientSock);
+        exit(1);
+    }
+    char *plaintext = strdup(buffer);
+
+    // Receive key
+    memset(buffer, '\0', BUFFER_SIZE);
+    charsRead = recv(clientSock, buffer, BUFFER_SIZE - 1, 0);
+    if (charsRead < 0)
+    {
+        error("ERROR reading from socket");
+        close(clientSock);
+        free(plaintext);
+        exit(1);
+    }
+    char *key = strdup(buffer);
+
+    // Encrypt plaintext using key
+    char *ciphertext = encrypt(plaintext, key);
+
+    // Send ciphertext back to client
+    charsRead = send(clientSock, ciphertext, strlen(ciphertext), 0);
+    if (charsRead < 0)
+    {
+        error("ERROR writing to socket");
+        close(clientSock);
+        free(plaintext);
+        free(key);
+        free(ciphertext);
+        exit(1);
+    }
+
+    // Clean up
+    close(clientSock);
+    free(plaintext);
+    free(key);
+    free(ciphertext);
+}
+
+int main(int argc, char *argv[])
+{
+    // Validate command-line arguments
+    validateArgs(argc, argv);
+
+    // Setup socket
+    int portNum = atoi(argv[1]);
+    int listenSock = setupSocket(portNum);
+
+    // Daemonize process
+    daemonize();
+
+    // Listen for incoming connections
+    while (1)
+    {
+        struct sockaddr_in clientAddr;
+        socklen_t clientLen = sizeof(clientAddr);
+        int clientSock = accept(listenSock, (struct sockaddr *)&clientAddr, &clientLen);
+
+        if (clientSock < 0)
         {
-            while (1)
-            {
-                newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-                if (newsockfd < 0)
-                {
-                    error("ERROR on accept");
-                }
-
-                char buffer[256];
-                bzero(buffer, 256);
-                int n = read(newsockfd, buffer, 255);
-                if (n < 0)
-                {
-                    error("ERROR reading from socket");
-                }
-
-                if (strcmp(buffer, "enc_client") != 0)
-                {
-                    fprintf(stderr, "Connection from invalid client\n");
-                    close(newsockfd);
-                    continue;
-                }
-
-                bzero(buffer, 256);
-                n = read(newsockfd, buffer, 255);
-                if (n < 0)
-                {
-                    error("ERROR reading from socket");
-                }
-                char *plaintext = strdup(buffer);
-
-                bzero(buffer, 256);
-                n = read(newsockfd, buffer, 255);
-                if (n < 0)
-                {
-                    error("ERROR reading from socket");
-                }
-                char *key = strdup(buffer);
-
-                if (strlen(plaintext) > strlen(key))
-                {
-                    fprintf(stderr, "Key length is too short\n");
-                    close(newsockfd);
-                    free(plaintext);
-                    free(key);
-                    continue;
-                }
-
-                char *ciphertext = encrypt(plaintext, key);
-                n = write(newsockfd, ciphertext, strlen(ciphertext));
-                if (n < 0)
-                {
-                    error("ERROR writing to socket");
-                }
-                free(plaintext);
-                free(key);
-                free(ciphertext);
-                close(newsockfd);
-            }
-            exit(0);
+            error("ERROR on accept");
+            continue;
         }
-        else if (pid < 0)
+
+        // Handle the connection in a separate process
+        pid_t pid = fork();
+        if (pid < 0)
         {
             error("ERROR on fork");
+            close(clientSock);
+            continue;
+        }
+        else if (pid == 0)
+        {
+            close(listenSock);
+            handleConnection(clientSock);
+            close(clientSock);
+            exit(0);
+        }
+        else
+        {
+            close(clientSock);
         }
     }
 
-    close(sockfd);
+    // Close the listening socket
+    close(listenSock);
 
     return 0;
 }
