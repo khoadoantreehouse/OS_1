fb6e2c55b4bfd4b1d9baaacecc7d0e0da6022ae0
khoadoantreehouse <khoa.doan@treehouse.finance>
2023-03-06 17:07:14 -0800

    again

--- doankh_program4/enc_server.c
+++ doankh_program4/enc_server.c
@@ -110,7 +110,7 @@ int main(int argc, char *argv[])
 
             if (strcmp(buffer, "enc_client") != 0)
             {
-                printf("%d\n", strcmp(buffer, "enc_client"));
+                printf("%s\n", buffer);
                 error("Error: not a valid client");
             }
 
