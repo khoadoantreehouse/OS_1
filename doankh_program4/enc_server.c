aec6b3b648334713840d08fc804e47ec06833257
khoadoantreehouse <khoa.doan@treehouse.finance>
2023-03-06 16:01:53 -0800

    re_do

--- doankh_program4/enc_server.c
+++ doankh_program4/enc_server.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <ctype.h>
+#include <unistd.h>
+#include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 
-#define MAX_CLIENTS 5
+#define MAX_CONNECTIONS 5
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+char *encrypt(char *message, char *key)
+{
+    char *ciphertext = (char *)malloc((strlen(message) + 1) * sizeof(char));
+    int i;
+    for (i = 0; i < strlen(message); i++)
+    {
+        int messageVal = message[i] - 'A';
+        int keyVal = key[i] - 'A';
+        if (messageVal == -33)
+            messageVal = 26;
+        if (keyVal == -33)
+            keyVal = 26;
+        int sum = messageVal + keyVal;
+        int cipherVal = sum % 27;
+        ciphertext[i] = cipherVal + 'A';
+        if (cipherVal == 26)
+            ciphertext[i] = ' ';
+    }
+    ciphertext[i] = '\0';
+    return ciphertext;
+}
 
 void error(const char *msg)
 {
@@ -39,121 +66,115 @@ char *encrypt(char *message, char *key)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc < 2)
     {
-        error("Usage: enc_server <listening_port>");
+        fprintf(stderr, "Usage: %s port\n", argv[0]);
+        exit(1);
     }
 
-    int port = atoi(argv[1]);
-    if (port <= 0)
+    int portno = atoi(argv[1]);
+    if (portno == 0)
     {
-        error("Invalid listening port");
+        error("Invalid port number");
     }
 
-    // create socket
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd, newsockfd;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
-        error("Error opening socket");
+        error("ERROR opening socket");
     }
 
-    // set SO_REUSEADDR option
-    int optval = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
-
-    // bind socket to port
-    struct sockaddr_in serv_addr, cli_addr;
-    memset((char *)&serv_addr, 0, sizeof(serv_addr));
+    bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(port);
+    serv_addr.sin_port = htons(portno);
+
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        error("Error binding socket to port");
+        error("ERROR on binding");
     }
 
-    // listen for connections
-    listen(sockfd, 5);
+    listen(sockfd, MAX_CONNECTIONS);
 
-    // handle client connections
-    socklen_t clilen = sizeof(cli_addr);
-    int clientfd, pid;
-    char buffer[2048];
-    char *key, ciphertext, encrypted;
-    int n, i;
-
-    while (1)
+    clilen = sizeof(cli_addr);
+    int i;
+    for (i = 0; i < MAX_CONNECTIONS; i++)
     {
-        // accept connection
-        clientfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-        if (clientfd < 0)
+        pid_t pid = fork();
+        if (pid == 0)
         {
-            error("Error accepting connection");
-        }
-
-        // fork child process to handle client
-        pid = fork();
-        if (pid < 0)
-        {
-            error("Error forking child process");
-        }
-        else if (pid == 0)
-        {
-            // child process
-
-            // verify client identity
-            memset(buffer, 0, 2048);
-            n = read(clientfd, buffer, 2047);
-            if (n < 0)
+            while (1)
             {
-                error("Error reading from socket");
-            }
+                newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+                if (newsockfd < 0)
+                {
+                    error("ERROR on accept");
+                }
 
-            if (strcmp(buffer, "enc_client") != 0)
-            {
-                printf("%d\n", strcmp(buffer, "enc_client"));
-                error("Error: not a valid client");
-            }
+                char buffer[256];
+                bzero(buffer, 256);
+                int n = read(newsockfd, buffer, 255);
+                if (n < 0)
+                {
+                    error("ERROR reading from socket");
+                }
 
-            // receive plaintext and key
-            memset(buffer, 0, 2048);
-            n = read(clientfd, buffer, 2047);
-            if (n < 0)
-            {
-                error("Error reading from socket");
-            }
-            strcpy(ciphertext, buffer);
+                if (strcmp(buffer, "enc_client") != 0)
+                {
+                    fprintf(stderr, "Connection from invalid client\n");
+                    close(newsockfd);
+                    continue;
+                }
 
-            memset(key, 0, 2048);
-            n = read(clientfd, key, 2047);
-            if (n < 0)
-            {
-                error("Error reading from socket");
-            }
+                bzero(buffer, 256);
+                n = read(newsockfd, buffer, 255);
+                if (n < 0)
+                {
+                    error("ERROR reading from socket");
+                }
+                char *plaintext = strdup(buffer);
 
-            // perform encryption
-            encrypted = encrypt(ciphertext, key);
-            // send ciphertext back to client
-            n = write(clientfd, encrypted, strlen(encrypted));
-            if (n < 0)
-            {
-                error("Error writing to socket");
-                // Close the socket for this client
-            }
+                bzero(buffer, 256);
+                n = read(newsockfd, buffer, 255);
+                if (n < 0)
+                {
+                    error("ERROR reading from socket");
+                }
+                char *key = strdup(buffer);
 
-            // close the client socket and exit child process
-            close(clientfd);
+                if (strlen(plaintext) > strlen(key))
+                {
+                    fprintf(stderr, "Key length is too short\n");
+                    close(newsockfd);
+                    free(plaintext);
+                    free(key);
+                    continue;
+                }
+
+                char *ciphertext = encrypt(plaintext, key);
+                n = write(newsockfd, ciphertext, strlen(ciphertext));
+                if (n < 0)
+                {
+                    error("ERROR writing to socket");
+                }
+                free(plaintext);
+                free(key);
+                free(ciphertext);
+                close(newsockfd);
+            }
             exit(0);
         }
-        else
+        else if (pid < 0)
         {
-            // in parent process
-            // close the client socket in the parent process to allow for more connections
-            close(clientfd);
+            error("ERROR on fork");
         }
     }
 
-    // close the server socket
     close(sockfd);
+
     return 0;
-} // added this line to fix the expected '}' error
+}
