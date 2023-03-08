afd5e1be9cfcc86de7ca31b2ef412d128fc6ee2a
khoadoantreehouse <khoa.doan@treehouse.finance>
2023-03-07 22:13:08 -0800

    acb

--- doankh_program4/enc_server.c
+++ doankh_program4/enc_server.c
@@ -120,7 +120,8 @@ int main(int argc, char *argv[])
             // child process
             char received_string[BUFFER_SIZE];
             // Receive data from client until the character "]" is reached
-            int n;
+            int n = recv(clientfd, buffer, BUFFER_SIZE, 0);
+            printf("%d\n", n);
             while ((n = recv(clientfd, buffer, BUFFER_SIZE, 0)) < 0)
             {
                 strncat(received_string, buffer, n);
